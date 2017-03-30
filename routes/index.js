a98ea57c55ead2eac6269b5558cb3627c80beec0
Jose Chavez Dominguez <josechavezdominguez@LG14s-MacBook-Pro.local>
2017-03-30 15:24:04 -0700

    Add getAllTodos function in Max's suggested way for readibility

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,23 @@ var router = express.Router();
 db = require('../queries');
 
 router.use(express.static('public'));
-router.get('/api/todos', db.getAllTodos);
+router.get('/api/todos', (req, res, next) => {
+  db.getAllTodos()
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved all items'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+);
+
 router.get('/api/todos/:id', db.getTodo);
 router.post('/api/todos', db.addTodo);
 router.put('/api/todos/:id', db.updateTodo);
